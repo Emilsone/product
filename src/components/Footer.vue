fbad5986ec82fdd2bb9dc0532f7d7b8ee071cd49
Emilsone <lawsonezekiel68@gmail.com>
2021-04-28 14:35:26 -0700

    create suscribe page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,21 +7,14 @@
         <div class="flex-1 px-4 py-6">
           <p class="uppercase md:mb-6 font-bold">Features</p>
           <ul class="list-reset mb-6">
-           
             <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
               <a href="#" class="no-underline">About Us </a>
             </li>
-             <li class="mt-2 inline-block mr-2 md:block md:mr-0 py-2">
-              <a href="#" class="no-underline"
-                >Farm Products
-                </a
-              >
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0 py-2">
+              <a href="#" class="no-underline">Farm Products </a>
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0 py-2">
-              <a href="#" class="no-underline"
-                >Our Teams
-               </a
-              >
+              <a href="#" class="no-underline">Our Teams </a>
             </li>
             <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
               <a href="#" class="no-underline"
@@ -42,25 +35,29 @@
             <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
               <a href="#" class="no-underline">Privacy</a>
             </li>
-             <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
+            <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
               <a href="#" class="no-underline">Contact Us</a>
             </li>
           </ul>
         </div>
 
-        <div class="flex-1  mb-6  py-6">
+        <div class="flex-1 mb-6 py-6">
           <p class="uppercase md:mb-6 font-bold py-2">Suscribe</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
-              <a href="#" class="no-underline">Managie@gmail.com</a>
-            </li>
-            <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
-              <a href="#" class="no-underline">Contact Form</a>
-            </li>
-            <li class="mt-2 block mr-2 md:block md:mr-0 py-2">
-              <a href="#" class="no-underline">+2347085674917</a>
-            </li>
-          </ul>
+          <!-- component -->
+          <div>
+            <form class="m-2 flex">
+              <input
+                class="p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+                placeholder="your@mail.com"
+              />
+              <button
+                class="px-4 bg-yellow-400 text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r"
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+         
         </div>
       </div>
       <!-- <FooterInfo /> -->
@@ -85,6 +82,10 @@ export default {
   color: white;
 }
 .blog-text {
-  color: #66d4d7;
+  color: #3d9421;
+}
+.btn-sign {
+  background-color: #3d9421;
+  border-radius: 8px;
 }
 </style>
