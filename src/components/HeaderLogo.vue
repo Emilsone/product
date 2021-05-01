6a84f36b6bc543e7b24e80ad5023ee5adefc03bc
Emilsone <lawsonezekiel68@gmail.com>
2021-05-01 12:32:42 -0700

    add header logo

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -1,21 +1,17 @@
 <template>
-  <a
-    :class="classList"
-    class="no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-    href="#"
-    >Farmpro<span class="text-4xl word-spacing dot-text">.</span>
-  </a>
-
-
-
-
-
-
-
-
-
-
-  
+  <div>
+    <img
+      alt="testimonial"
+      class="w-20 h-20 mb-8 object-cover object-center inline-block"
+      src="../assets/header-logo.png"
+    />
+    <a
+      :class="classList"
+      class="no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+      href="#"
+      >Farmpro<span class="text-4xl word-spacing dot-text">.</span>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -46,6 +42,6 @@ export default {
 </script>
 <style scoped>
 .dot-text {
- color: #3d9421;
+  color: #3d9421;
 }
 </style>
