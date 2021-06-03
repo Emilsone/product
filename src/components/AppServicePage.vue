55b0c694d747f8978b08f6897e77c09cf8c2436b
Emilsone <lawsonezekiel68@gmail.com>
2021-06-03 16:25:12 -0700

    create cart page

---
+++ src/components/AppServicePage.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      
+      <div class="flex flex-wrap -m-4">
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6 ">
+            <div
+              class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <h1 class="text-lg text-gray-900 font-bold title-font mb-2">
+              Free Shipping
+            </h1>
+            <p class="leading-relaxed text-base">
+              Fingerstache flexitarian street art 8-bit waist co, subway tile
+              poke farm.
+            </p>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6 ">
+            <div
+              class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="6" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path
+                  d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"
+                ></path>
+              </svg>
+            </div>
+            <h1 class="text-lg text-gray-900 font-bold title-font mb-2">
+              14 Days Easy Return
+            </h1>
+            <p class="leading-relaxed text-base">
+              Fingerstache flexitarian street art 8-bit waist co, subway tile
+              poke farm.
+            </p>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 p-4">
+          <div class="border border-gray-200 p-6 ">
+            <div
+              class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4"
+            >
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <h1 class="text-lg text-gray-900 font-bold title-font mb-2">
+              24/7 Customer Support
+            </h1>
+            <p class="leading-relaxed text-base">
+              Fingerstache flexitarian street art 8-bit waist co, subway tile
+              poke farm.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
